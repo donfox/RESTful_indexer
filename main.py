d84171da8ce30ca10834bd13be168fb14732e3e6
donfox1 <donfox1@mac.com>
2024-11-12 21:36:48 -0500

    Added exception handeling and doc statements.

--- main.py
+++ main.py
@@ -21,44 +21,45 @@ import redis_utils
 import config
 import logging
 
-def detect_and_fetch_missing_blocks(redis_conn):
+def detect_and_fetch_missing_blocks(redis_conn) -> None:
     """
     Detects and processes missing blockchain blocks.
 
-    This function first detects missing blocks from the `processed_blocks` set in Redis
-    by calling the `block_utils.detect_missing_blocks()` function. Then, it fetches and 
-    processes those missing blocks by calling `block_utils.request_missing_blocks()`.
-
+    This function detects missing blocks from the `processed_blocks` set in Redis
+    and fetches them using utility functions.
     Args:
         redis_conn (redis.Redis): Redis connection object to interact with the Redis database.
 
     Returns:
         None
     """
-    missing_blocks = block_utils.detect_missing_blocks(redis_conn)
-
-    if missing_blocks:
-        block_utils.request_missing_blocks(missing_blocks, redis_conn)
-    
-    logging.info(f"MISSING BLOCKS: {missing_blocks}")
-
-
+    try:
+        missing_blocks = block_utils.detect_missing_blocks(redis_conn)
+        if missing_blocks:
+            block_utils.request_missing_blocks(missing_blocks, redis_conn)
+            logging.info(f"MISSING BLOCKS: {missing_blocks}")
+        else:
+            logging.info("No missing blocks detected.")
+    except Exception as e:
+        logging.error(f"Error detecting or fetching missing blocks: {e}")
+        
 def main():
     """
-    Main function to initialize Redis connection and process blockchain blocks.
+    Iinitialize Redis connection and process blockchain blocks.
 
-    This function establishes a connection to Redis, detects and processes any 
-    missing blockchain blocks, then extracts the latest blockchain blocks by 
-    calling the appropriate utility functions from `block_utils` and `redis_utils`.
+    Establishes a connection to Redis, processes missing blocks, and extracts
+    the latest blockchain blocks.
 
     Returns:
         None
     """
-    redis_conn = redis_utils.get_redis_connection()
+    try:
+        redis_conn = redis_utils.get_redis_connection()
+        detect_and_fetch_missing_blocks(redis_conn)
+        block_utils.extract_current_blocks(redis_conn)
+    except Exception as e:
+        logging.error(f"Error in main processing: {e}")
 
-    detect_and_fetch_missing_blocks(redis_conn)
-    block_utils.extract_current_blocks(redis_conn)
-        
 
 if __name__ == '__main__':
     main()
